f9cf50f770ee935f403f5436fff36dd34716cfe7
Pratik Deshmukh <pratik.deshmukh@calsoftinc.com>
2020-08-05 13:30:55 +0530

    Added capitalization code in string manipulation file

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,5 +1,6 @@
-"""Rreversing a string."""
+"""Reversing a string."""
 
 input_string = input("\n Enter a string : ")
 reversed_string = input_string[::-1]
 print(f'Reverse of {input_string} is {reversed_string}')
+print('Capitalized reverse string : ', reversed_string.capitalize())
